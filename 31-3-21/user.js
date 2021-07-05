d3f406fd7428c6e957ad878c8fb17bd05a1f4494
1NT18IS115 <83122905+1nt18is115@users.noreply.github.com>
2021-07-05 10:15:27 +0530

    user details

---
+++ 31-3-21/user.js
@@ -0,0 +1,7 @@
+function takeInput() {
+    var uname = document.getElementById("Name-id").value;
+    var uage = document.getElementById("age-id").value;
+    console.log("User entered name as " + uname);
+    console.log("User entered age as " + uage);
+    
+}
