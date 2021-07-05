d560c870c191d8ab6993c7bce18f93c17bd6c879
1NT18IS115 <83122905+1nt18is115@users.noreply.github.com>
2021-07-05 10:12:55 +0530

    Castle

---
+++ 7-4-21/castle.js
@@ -0,0 +1,15 @@
+function outerSingleClickFunc()  {
+    alert("Entered the outer ring");
+}
+
+function innerSingleClickFunc()  {
+    alert("Entered the 2nd ring");
+}
+
+function innerRingClickFunc()   {
+    alert("Entered the innermost ring");
+}
+
+function escapeFunc()   {
+    alert("Escaped!");
+}
